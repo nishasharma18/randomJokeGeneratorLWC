e3403fc614c37b0cc35ac72e20df6a6805b7de10
Nisha Sharma <sunshine.kapoor270@gmail.com>
2023-02-09 13:44:28 +0530

    Update randomJokeGenerator.js

--- randomJokeGenerator/randomJokeGenerator.js
+++ randomJokeGenerator/randomJokeGenerator.js
@@ -8,9 +8,7 @@ export default class RandomJokeGenerator extends LightningElement {
     @wire(getJokeApiSettings, {})
     getJokeApiSettings({ error, data }) {
         if (data) {
-            console.log('data: '+ JSON.stringify(data));
             this.calloutURI = data.URL__c;
-            console.log('calloutURI 2: ' + this.calloutURI);
             this.fetchJoke();
         } else if (error) {
             console.error(error);
@@ -22,14 +20,11 @@ export default class RandomJokeGenerator extends LightningElement {
     }
  
     fetchJoke() {
-        //const calloutURI = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single';
         fetch(this.calloutURI, {
             method: "GET"
         }).then((response) => response.json())
         .then((repos) => {
-            console.log('repos:' + JSON.stringify(repos))
             this.jokeData = repos.joke;
-            console.log(this.jokeData);
         });
     }
 
